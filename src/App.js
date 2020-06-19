33a4599953885deec6adca91822c091f0ac9788c
sudo-dax <dax.scholtz@gmail.com>
2020-06-20 02:31:51 +1000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import CurrencySelector from './components/CurrencySelector'
 import DataDisplay from './components/DataDisplay'
 
@@ -12,6 +12,16 @@ const App = (props) => {
   const defaultCurrency = "AUD"
   const [currency, setCurrency] = useState(defaultCurrency)
   const [bitcoinData, setBitcoinData] = useState({})
+
+  useEffect(() => {
+    console.log("in useEffect")
+    // componentDidMount
+    const data = {"2020-01-10":11866.0075,"2020-01-11":11619.6983,"2020-01-12":11858.2603}
+    setBitcoinData(data)
+    // Override componentWillUnmount with the returned callback function
+    return () => {console.log("This executes on componentWillUnmount")}
+  }, [currency])
+
 
   function handleCurrencyChange(newCurrency) {
     setCurrency(newCurrency)
